de20a86722d23f2ab9bc16d84cdf4999e17be31a
yuanyu90221 <yuanyu90221@gmail.com>
2019-04-05 03:21:25 +0800

    add component setState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Button 
+        activeText="ON"
+        inActiveText="OFF"   
+      />
+    )
   }
 }
 
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activated: false
+    };
+    this.handleActiveChange = this.handleActiveChange.bind(this);
+  }
+  handleActiveChange() {
+    // this.setState({activated: !this.state.activated});
+    this.setState((prevState)=>{
+      return {
+        activated: !prevState.activated
+      }
+    });
+  }
+  render() {
+    const buttonText = this.state.activated? this.props.activeText:this.props.inActiveText;
+    return (
+      <button onClick={this.handleActiveChange}>
+        {buttonText}
+      </button>
+    )
+  }
+}
 export default App;
