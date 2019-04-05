2452c8735e86440fe2fc01c7c8ca422bd7c6e337
yuanyu90221 <yuanyu90221@gmail.com>
2019-04-06 03:35:05 +0800

    add useState to reduce stateful component to stateless component with hook

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <Button 
-        activeText="ON"
-        inActiveText="OFF"   
-      />
-    )
-  }
+function App() {
+  // false is the initial state
+  const [ activated, setActivated ] = useState(false);
+  const buttonText = activated? 'Active': 'Inactive';
+  const onClick = () => setActivated(!activated);
+  return (
+    <button onClick={onClick}>
+      { buttonText }
+    </button>
+  )
 }
+// class App extends Component {
+//   render() {
+//     return (
+//       <Button 
+//         activeText="ON"
+//         inActiveText="OFF"   
+//       />
+//     )
+//   }
+// }
 
-class Button extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activated: false
-    };
-    this.handleActiveChange = this.handleActiveChange.bind(this);
-  }
-  handleActiveChange() {
-    // this.setState({activated: !this.state.activated});
-    this.setState((prevState)=>{
-      return {
-        activated: !prevState.activated
-      }
-    });
-  }
-  render() {
-    const buttonText = this.state.activated? this.props.activeText:this.props.inActiveText;
-    return (
-      <button onClick={this.handleActiveChange}>
-        {buttonText}
-      </button>
-    )
-  }
-}
+// class Button extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       activated: false
+//     };
+//     this.handleActiveChange = this.handleActiveChange.bind(this);
+//   }
+//   handleActiveChange() {
+//     // this.setState({activated: !this.state.activated});
+//     this.setState((prevState)=>{
+//       return {
+//         activated: !prevState.activated
+//       }
+//     });
+//   }
+//   render() {
+//     const buttonText = this.state.activated? this.props.activeText:this.props.inActiveText;
+//     return (
+//       <button onClick={this.handleActiveChange}>
+//         {buttonText}
+//       </button>
+//     )
+//   }
+// }
 export default App;
