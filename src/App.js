a7c6085ccc0d496753462535cd5df3f4a0454389
yuanyu90221 <yuanyu90221@gmail.com>
2019-04-07 03:25:25 +0800

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  // false is the initial state
-  const [ activated, setActivated ] = useState(false);
-  const buttonText = activated? 'Active': 'Inactive';
-  const onClick = () => setActivated(!activated);
+  const [count, setCount] = useState(0);
+  const handleIncrease = ()=> setCount(count + 1);
+  const handleDecrease = ()=> setCount(count - 1);
+  useEffect(()=>{
+    console.log(`I'm inside the useEffect function. The current count is ${count}`);
+  })
   return (
-    <button onClick={onClick}>
-      { buttonText }
-    </button>
+    <div>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+      <h1>{count}</h1>
+    </div>
   )
 }
-// class App extends Component {
-//   render() {
-//     return (
-//       <Button 
-//         activeText="ON"
-//         inActiveText="OFF"   
-//       />
-//     )
-//   }
-// }
 
-// class Button extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       activated: false
-//     };
-//     this.handleActiveChange = this.handleActiveChange.bind(this);
-//   }
-//   handleActiveChange() {
-//     // this.setState({activated: !this.state.activated});
-//     this.setState((prevState)=>{
-//       return {
-//         activated: !prevState.activated
-//       }
-//     });
-//   }
-//   render() {
-//     const buttonText = this.state.activated? this.props.activeText:this.props.inActiveText;
-//     return (
-//       <button onClick={this.handleActiveChange}>
-//         {buttonText}
-//       </button>
-//     )
-//   }
-// }
 export default App;
