73508284bacf565b89dc7a3c9e8699d271c17cc2
yuanyu90221 <yuanyu90221@gmail.com>
2019-04-07 03:52:05 +0800

    add return function in UseEffect to execute next time useState change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { useState, useEffect } from 'react';
-
-function App() {
+function Counter() {
   const [count, setCount] = useState(0);
   const handleIncrease = ()=> setCount(count + 1);
   const handleDecrease = ()=> setCount(count - 1);
   useEffect(()=>{
     console.log(`I'm inside the useEffect function. The current count is ${count}`);
-  })
+    
+    return () => {
+      console.log(`I'm removing anything that was setup above!, The last count was ${count}`);
+    };
+  });
   return (
     <div>
       <button onClick={handleIncrease}>Increase</button>
       <button onClick={handleDecrease}>Decrease</button>
       <h1>{count}</h1>
+    </div>
+  )
+}
+function App() {
+  const [visible, setVisible] = useState(false);
+  const handleShowHide = ()=>setVisible(!visible);
+  return (
+    <div>
+      <button onClick={handleShowHide}>
+        Show / Hide the Counter Component
+      </button>
+      {visible&&<Counter/>}
     </div>
   )
 }
