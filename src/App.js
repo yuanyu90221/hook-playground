bc76188ee1bc864ee24e9f15b948ecc744ae7d86
yuanyu90221 <yuanyu90221@gmail.com>
2019-04-08 00:17:13 +0800

    useContext to reduce the processs of contextProvider to contextConsumer pass data

--- src/App.js
+++ src/App.js
@@ -1,50 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext, createContext } from 'react';
 
+const NameContext = createContext();
 function App() {
-  // false is the initial state
-  const [ activated, setActivated ] = useState(false);
-  const buttonText = activated? 'Active': 'Inactive';
-  const onClick = () => setActivated(!activated);
+  const [name, setName] = useState('Billy Shapespeare');
+
+  return (<div>
+    <NameContext.Provider value={name}>
+      <Child />
+    </NameContext.Provider>
+  </div>);
+}
+
+function Child() {
   return (
-    <button onClick={onClick}>
-      { buttonText }
-    </button>
+    <section className="child">
+      <GrandChild/>
+    </section>
   )
 }
-// class App extends Component {
-//   render() {
-//     return (
-//       <Button 
-//         activeText="ON"
-//         inActiveText="OFF"   
-//       />
-//     )
-//   }
-// }
 
-// class Button extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       activated: false
-//     };
-//     this.handleActiveChange = this.handleActiveChange.bind(this);
-//   }
-//   handleActiveChange() {
-//     // this.setState({activated: !this.state.activated});
-//     this.setState((prevState)=>{
-//       return {
-//         activated: !prevState.activated
-//       }
-//     });
-//   }
-//   render() {
-//     const buttonText = this.state.activated? this.props.activeText:this.props.inActiveText;
-//     return (
-//       <button onClick={this.handleActiveChange}>
-//         {buttonText}
-//       </button>
-//     )
-//   }
-// }
+function GrandChild() {
+  return (
+    <div class="grandChild">
+      <Button/>
+    </div>
+  )
+}
+function Button() {
+  const name = useContext(NameContext);
+  return (
+    <button>{name}</button>
+  )
+}
+
 export default App;
