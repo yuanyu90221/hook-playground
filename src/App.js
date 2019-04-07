afa48c970e5d1536b72facba598ce5520e31bd94
yuanyu90221 <yuanyu90221@gmail.com>
2019-04-07 23:06:17 +0800

    invoke useEffect with event

--- src/App.js
+++ src/App.js
@@ -1,50 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 
 function App() {
-  // false is the initial state
-  const [ activated, setActivated ] = useState(false);
-  const buttonText = activated? 'Active': 'Inactive';
-  const onClick = () => setActivated(!activated);
+  const [userText, setUserText] = useState('');
+  const handleUserKeyPress = (event)=>{
+    const { key, keyCode } = event;
+    // lowercase alphabet
+    if (keyCode === 32 || (keyCode >= 65 && keyCode <= 90)){
+      setUserText(`${userText}${key}`);
+    }
+  };
+
+  useEffect(()=>{
+    window.addEventListener('keydown', handleUserKeyPress);
+    return ()=> {
+      window.removeEventListener('keydown', handleUserKeyPress);
+    }
+  });
   return (
-    <button onClick={onClick}>
-      { buttonText }
-    </button>
+    <div>
+      <h1>Feel free to type!Your text will show up below </h1>
+      <blockquote>
+        {userText}
+      </blockquote>
+    </div>
   )
 }
-// class App extends Component {
-//   render() {
-//     return (
-//       <Button 
-//         activeText="ON"
-//         inActiveText="OFF"   
-//       />
-//     )
-//   }
-// }
 
-// class Button extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       activated: false
-//     };
-//     this.handleActiveChange = this.handleActiveChange.bind(this);
-//   }
-//   handleActiveChange() {
-//     // this.setState({activated: !this.state.activated});
-//     this.setState((prevState)=>{
-//       return {
-//         activated: !prevState.activated
-//       }
-//     });
-//   }
-//   render() {
-//     const buttonText = this.state.activated? this.props.activeText:this.props.inActiveText;
-//     return (
-//       <button onClick={this.handleActiveChange}>
-//         {buttonText}
-//       </button>
-//     )
-//   }
-// }
 export default App;
