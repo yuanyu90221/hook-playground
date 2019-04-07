363c93e017dfa13f06edc68821b64ab93f731362
yuanyu90221 <yuanyu90221@gmail.com>
2019-04-08 01:16:09 +0800

    add reuse hooks to reduce code

--- src/App.js
+++ src/App.js
@@ -1,62 +1,31 @@
 import React, { useState } from 'react';
 
+function useGNInput(initValue) {
+  const [value, setValue] = useState(initValue);
+  const onChange = (event) => setValue(event.target.value);
+  return {value, onChange};
+}
+
 function App() {
-  const [ count, setCount ] = useState(0);
-  // [version-1]update the value with pass one value
-  // const handleClick = () => setCount(count+1);
-  // [version-1]update the value with callback function
-  const handleIncrease = () => setCount((prevCount)=>prevCount+1);
-  const handleDecrease = () => setCount((prevCount)=>prevCount-1);
-  const handleReset = () => setCount(0);
   return (
-    <div>
-      <button onClick={handleIncrease}>
-        Increase    
-      </button>
-      <button onClick={handleDecrease}>
-        Decrease    
-      </button>
-      <button onClick={handleReset}>
-        Reset
-      </button>
-      <h1>{count}</h1>
-    </div>
+    <form>
+      <input
+        type="text"
+        placeholder="Name"
+        {...useGNInput('')}
+      />
+      <input
+        type="text"
+        placeholder="SurName"
+        {...useGNInput('')}
+      />
+      <input
+        type="number"
+        placeholder="Age"
+        {...useGNInput(0)}
+      />
+    </form>
   )
 }
-// class App extends Component {
-//   render() {
-//     return (
-//       <Button 
-//         activeText="ON"
-//         inActiveText="OFF"   
-//       />
-//     )
-//   }
-// }
 
-// class Button extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       activated: false
-//     };
-//     this.handleActiveChange = this.handleActiveChange.bind(this);
-//   }
-//   handleActiveChange() {
-//     // this.setState({activated: !this.state.activated});
-//     this.setState((prevState)=>{
-//       return {
-//         activated: !prevState.activated
-//       }
-//     });
-//   }
-//   render() {
-//     const buttonText = this.state.activated? this.props.activeText:this.props.inActiveText;
-//     return (
-//       <button onClick={this.handleActiveChange}>
-//         {buttonText}
-//       </button>
-//     )
-//   }
-// }
 export default App;
