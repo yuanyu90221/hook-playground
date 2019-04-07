b20355911f7c8ba74f32b22c68fb8c1c526ef9b6
yuanyu90221 <yuanyu90221@gmail.com>
2019-04-08 00:50:16 +0800

    reuse the hooks function we define in different component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 
-function App() {
-  const [ count, setCount ] = useState(0);
-  // [version-1]update the value with pass one value
-  // const handleClick = () => setCount(count+1);
-  // [version-1]update the value with callback function
-  const handleIncrease = () => setCount((prevCount)=>prevCount+1);
-  const handleDecrease = () => setCount((prevCount)=>prevCount-1);
-  const handleReset = () => setCount(0);
+function useCounter(initalValue) {
+  const [ count, setCount ] = useState(initalValue);
+  const handleIncrease = () => setCount(count+1);
+  const handleDecrease = () => setCount(count-1);
+  const handleReset = () => setCount(initalValue);
+  return {count, handleIncrease, handleDecrease, handleReset};
+}
+function Display(props) {
+  const {count, handleIncrease, handleDecrease, handleReset} = useCounter(props.start);
   return (
     <div>
       <button onClick={handleIncrease}>
@@ -23,40 +24,33 @@ function App() {
     </div>
   )
 }
-// class App extends Component {
-//   render() {
-//     return (
-//       <Button 
-//         activeText="ON"
-//         inActiveText="OFF"   
-//       />
-//     )
-//   }
-// }
 
-// class Button extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       activated: false
-//     };
-//     this.handleActiveChange = this.handleActiveChange.bind(this);
-//   }
-//   handleActiveChange() {
-//     // this.setState({activated: !this.state.activated});
-//     this.setState((prevState)=>{
-//       return {
-//         activated: !prevState.activated
-//       }
-//     });
-//   }
-//   render() {
-//     const buttonText = this.state.activated? this.props.activeText:this.props.inActiveText;
-//     return (
-//       <button onClick={this.handleActiveChange}>
-//         {buttonText}
-//       </button>
-//     )
-//   }
-// }
+function FancyDisplay(props) {
+  const {count, handleIncrease, handleDecrease, handleReset} = useCounter(props.start);
+  return (
+    <section>
+      <button onClick={handleIncrease}>
+        Increase    
+      </button>
+      <button onClick={handleDecrease}>
+        Decrease    
+      </button>
+      <button onClick={handleReset}>
+        Reset
+      </button>
+      <h2>{count}</h2>
+    </section>
+  )
+}
+function App() {
+  
+  return (
+    <div>
+      <Display start={10}/>
+      <Display start={2}/>
+      <FancyDisplay start={0}/>
+    </div>
+  )
+}
+
 export default App;
