ea9c44e65d1a5457d56453dc7724f6ac487d11f8
yuanyu90221 <yuanyu90221@gmail.com>
2019-04-07 22:31:48 +0800

    second params of useEffect is tell which state will be track

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from 'react';
 function Counter() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState('salmon');
   const handleIncrease = ()=> setCount(count + 1);
   const handleDecrease = ()=> setCount(count - 1);
+  const handleColorChange = ()=> {
+    const nextColor = color === 'salmon' ? 'gold': 'salmon';
+    setColor(nextColor);
+  }
   useEffect(()=>{
     console.log(`I'm inside the useEffect function.Will only run upon mounting, The current count is ${count}`);
     
     return () => {
-      console.log(`I'm removing anything that was setup above!, The last count was ${count}, But now , will only run when component is being umounted`);
+      console.log(`I'm removing anything that was setup above!, The last count was ${count}`);
     };
-  }, []);
+  }, [color]);
   return (
     <div>
       <button onClick={handleIncrease}>Increase</button>
       <button onClick={handleDecrease}>Decrease</button>
-      <h1>{count}</h1>
+      <button onClick={handleColorChange}>ChangeColor</button>
+      <h1 style={ {color} }>{count}</h1>
     </div>
   )
 }
